fa2d654760adcb40953efba8eb86b73afd448b3d
daniescobarx <daniela.mollo@sptech.school>
2024-03-07 14:00:28 -0300

    add cadastro/login usando JOptionPane

---
+++ loginuser/src/main/java/login/Login.java
@@ -0,0 +1,45 @@
+package login;
+import javax.swing.*;
+
+public class Login {
+    public static void main(String[] args) {
+        boolean logado = false;
+        String usuarioCadastrado = null;
+        String senhaCadastrado = null;
+
+        while (!logado){
+            String opcao = JOptionPane.showInputDialog(null, "Selecione uma opcao:\n 1 - Cadastrar\n2" +
+                    " - Logar\n 3 - Sair", "Menu - SafeLearn ", JOptionPane.PLAIN_MESSAGE);
+
+            if (opcao== null || opcao.equals("3")){
+                JOptionPane.showMessageDialog(null, "Saindo...");
+                break;
+            }
+
+            switch (opcao){
+                case "1":
+                    usuarioCadastrado = JOptionPane.showInputDialog(null, "Digite o nome do usúario:");
+                    senhaCadastrado = JOptionPane.showInputDialog(null, "Digite a senha");
+                    JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!!");
+                case "2":
+                    String usuario = JOptionPane.showInputDialog(null, "Digite o nome do usuário:");
+                    String senha = JOptionPane.showInputDialog(null, "Digite a senha: ");
+
+                    if (usuario != null && senha!=null &&
+                            usuario.equals(usuarioCadastrado) && senha.equals(senhaCadastrado)
+                    ){
+                        JOptionPane.showMessageDialog(null, "Login realizado!");
+                        logado = true;
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Usuário ou senha incorreto.\n Tente novamente");
+                    }
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcão invalida.\n Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
+
